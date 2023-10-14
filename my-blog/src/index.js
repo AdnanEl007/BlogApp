134c7bc3fd449638739db29e40027ab0b2ab4e19
Adnan <adnan.elouezzani@docdoku.com>
2023-10-14 23:22:10 +0200

    feat: add backend

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDi8nT8hVPSAtWbqpg1xUOkcNIsWBE-HMk",
+  authDomain: "my-react-blog-1ffe3.firebaseapp.com",
+  projectId: "my-react-blog-1ffe3",
+  storageBucket: "my-react-blog-1ffe3.appspot.com",
+  messagingSenderId: "339091450586",
+  appId: "1:339091450586:web:a88124afaed67152ac53e3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,13 +1,51 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
+import useUser from "../hooks/useUser";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams();
+  const { canUpvote } = articleInfo;
+
+  const { user, isLoading } = useUser();
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const token = user && (await user.getIdToken());
+      const headers = token ? { authtoken: token } : {};
+      const response = await axios.get(`/api/articles/${articleId}`, {
+        headers,
+      });
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+    if (isLoading) {
+      loadArticleInfo();
+    }
+  }, [isLoading, user]);
+
   //const params = useParams();
   //const articleId = params.articleId;
 
   const article = articles.find((article) => article.name === articleId);
+
+  const addUpvote = async () => {
+    const token = user && (await user.getIdToken());
+    const headers = token ? { authtoken: token } : {};
+    const response = await axios.put(
+      `/api/articles/${articleId}/upvote`,
+      null,
+      { headers }
+    );
+    const updatedArticle = response.data;
+    setArticleInfo(updatedArticle);
+  };
+
   if (!article) {
     return <NotFoundPage />;
   }
@@ -15,9 +53,28 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <div className="upvote-section">
+        {user ? (
+          <button onClick={addUpvote}>
+            {canUpvote ? "Upvote" : "Already upvoted"}
+          </button>
+        ) : (
+          <button>Log in to upvote</button>
+        )}
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      </div>
       {article.content.map((paragraph) => (
         <p key={paragraph}>{paragraph}</p>
       ))}
+      {user ? (
+        <AddCommentForm
+          articleName={articleId}
+          onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+        />
+      ) : (
+        <button>Log in to add a comment</button>
+      )}
+      <CommentsList comments={articleInfo.comments} />
     </>
   );
 };
